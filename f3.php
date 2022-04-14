660057d36e1cabb8e9cccbaf96f923e0d87f1ab8
aelmouny11 <elmoumny.azddine@gmail.com>
2022-04-14 22:37:26 +0000

    add tailwind

--- f3.php
+++ f3.php
@@ -38,6 +38,7 @@ function mention($note)
 <head>
     <meta charset="utf-8">
     <title>atelier 5</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body style="border: solid black;">
@@ -77,6 +78,53 @@ function mention($note)
             </a>';
     }
     ?>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Product name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Color
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Category
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Price
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php foreach ($tab_etudiants as $index => $value) : ?>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            
+                        </th>
+                        <td class="px-6 py-4">
+                            Sliver
+                        </td>
+                        <td class="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td class="px-6 py-4">
+                            $2999
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -7,22 +7,23 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body class="bg-slate-900">
-    <div class="w-2/3 mx-auto my-40"> <img src="fssm.png" alt="logo du FSSM" class="w-80">
-        <form action="fichphp.php" method="post" class="text-gray-200">
-            <div>
-                <label for="ident" class="uppercase">identifiant:</label><br>
-                <label for="pwd" class="uppercase">mot de passe:</label>
+<body class="bg-slate-600">
+    <div class="w-1/3 mx-auto my-40 bg-slate-700 p-8 shadow-gray-900 shadow-[8px_8px_5px]  rounded"> <img src="fssm.png" alt="logo du FSSM" class="w-60 h-28 drop-shadow-xl ">
+        <form action="fichphp.php" method="post" class="space-y-3 text-white">
+            <div class="grid grid-cols-3">
+                <label for="ident" class="uppercase ">identifiant:</label>
+                <input type="text" name="ident" class="col-span-2 rounded-md outline text-gray-600 " placeholder="username" />
+            </div>
+            <div class="grid grid-cols-3">
+                <label for="pwd" class="uppercase ">mot de passe:</label>
+                <input type="password" name="pwd" class="col-span-2 rounded-md outline text-gray-600" placeholder="password" />
             </div>
             <div>
-                <input type="text" name="ident" />
-                <input type="password" name="pwd">
-                <div>
-                    <input type="reset" value="Annuler" style="margin: 5px;padding:5px ;font-weight:bold; background-color: black;color: white;border-width: 3px;border: solid,black;">
-                    <input type="submit" value="Valider" style="margin: 5px;padding:5px ;font-weight:bold; background-color: black;color: white;border-width: 3px;border: solid,black;">
-                </div>
+                <input type="reset" value="Annuler" class="bg-red-500 py-2 px-3 focus:ring-2 focus:ring-red-400 focus:bg-red-700 shadow-md shadow-zinc-900">
+                <input type="submit" value="Valider" class="bg-green-600 py-2 px-3 focus:ring-2 focus:ring-green-400 focus:bg-green-800 shadow-md shadow-zinc-900">
             </div>
-        </form>
+    </div>
+    </form>
     </div>
 </body>
 
